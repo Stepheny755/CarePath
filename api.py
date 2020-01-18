b5d6492d6ae8cfcb500a00e0f980335d2a8e6ff8
Stephen Yang <stepheny755@gmail.com>
2020-01-18 18:58:13 -0500

    update

--- api.py
+++ api.py
@@ -40,33 +40,31 @@ class API():
         chatList=self.getUserData()['data']['chatsForOrganization']['chats']
         return chatList
 
-    def findChatByID(self,receiverID):
+    def findChatByMemberID(self,receiverID):
         for chat in a.parseChats():
             if(chat['type']=='single'):
                 if(chat['members'][0]['id']==receiver or chat['members'][1]['id']==receiver):
                     return chat['id']
 
-    def findChatByName(self,receiverName):
+    def findChatByMemberName(self,receiverName):
         for chat in a.parseChats():
             if(chat['type']=='single'):
                 if(chat['members'][0]['firstname']==receiverName or chat['members'][1]['firstname']==receiverName):
                     return chat['id']
+
+    def findChatByName():
 
     def readMessage(self,chatId):
         payload = "{\"query\":\"query fetchMessages ($chatId: ID!, $continuationId: Int, $limit: Int) {\\r\\n    chat(chatId: $chatId) {\\r\\n        messages(continuationId: $continuationId, limit: $limit) {\\r\\n            messages {\\r\\n                ...messageFields\\r\\n            }\\r\\n        }\\r\\n    }\\r\\n}\\r\\nfragment messageFields on Message {\\r\\n    id\\r\\n    priority\\r\\n    message\\r\\n    image\\r\\n    type\\r\\n    attachment {\\r\\n        ...AttachmentFragment\\r\\n    }\\r\\n    dateCreated\\r\\n    sender {\\r\\n        ...publicUserFields\\r\\n    }\\r\\n    deliveredTo {\\r\\n        ...deliveryReceiptFields\\r\\n    }\\r\\n    readBy {\\r\\n        ...readReceiptFields\\r\\n    }\\r\\n    data {\\r\\n        __typename ... on ConsultMessageData {\\r\\n            mrn\\r\\n            firstname\\r\\n            lastname\\r\\n            details\\r\\n        }\\r\\n    }\\r\\n}\\r\\n\\r\\nfragment readReceiptFields on ReadReceipt {\\r\\n    messageId\\r\\n    user {\\r\\n        ...publicUserFields\\r\\n    }\\r\\n    timestamp\\r\\n}\\r\\n\\r\\nfragment deliveryReceiptFields on DeliveryReceipt {\\r\\n    messageId\\r\\n    user {\\r\\n        ...publicUserFields\\r\\n    }\\r\\n    timestamp\\r\\n}\\r\\n\\r\\nfragment publicUserFields on PublicUser {\\r\\n    id\\r\\n    firstname\\r\\n    lastname\\r\\n    username\\r\\n    email\\r\\n    phonenumber\\r\\n    role\\r\\n    profilePic {\\r\\n        url\\r\\n    }\\r\\n    workStatus\\r\\n    statusDescription\\r\\n    workStatusProxy {\\r\\n        ...publicUserStatusFields\\r\\n    }\\r\\n}\\r\\n\\r\\nfragment publicUserStatusFields on PublicUser {\\r\\n    id\\r\\n    firstname\\r\\n    lastname\\r\\n    username\\r\\n    phonenumber\\r\\n    role\\r\\n    profilePic {\\r\\n        url\\r\\n    }\\r\\n}\\r\\n\\r\\nfragment AttachmentFragment on Attachment {\\r\\n    id\\r\\n    url\\r\\n    mimeType\\r\\n    fileName\\r\\n}\",\"variables\":{\"chatId\":\""+chatId+"\"}}"
         headers = {'hypercare-scope': self.scope,'Authorization': self.authorization,'Content-Type': 'application/json'}
         jsondata = requests.post(self.chaturi,headers=headers,data=payload)
         messageList = json.loads(jsondata.text)['data']['chat']['messages']['messages']
-        for i in (messageList):
-            print(i)
-            print()
-        return jsondata.text
+        return messageList
 
     def sendMessage(self,chatId,inputMessage):
         payload = "{\"query\":\"mutation sendMessage($chatId: ID!, $message: String!, $fileId: Int, $type: MessageType, $priority: Boolean) {\\r\\n    chat(chatId: $chatId) {\\r\\n        sendMessage(message: $message, type: $type, fileId: $fileId, priority: $priority) {\\r\\n            id\\r\\n            image\\r\\n            attachment {\\r\\n                ...AttachmentFragment\\r\\n            }\\r\\n            message\\r\\n            type\\r\\n            sender {\\r\\n                id\\r\\n                username\\r\\n            }\\r\\n        }\\r\\n    }\\r\\n}\\r\\n\\r\\nfragment AttachmentFragment on Attachment {\\r\\n    id\\r\\n    url\\r\\n    mimeType\\r\\n    fileName\\r\\n}\",\"variables\":{\"chatId\":\""+chatId+"\",\"message\":\""+inputMessage+"\",\"type\":\"text\",\"priority\":false}}"
         headers={'hypercare-scope':self.scope,'Authorization':self.authorization,'Content-Type': 'application/json'}
         jsondata = requests.post(self.chaturi,headers=headers,data=payload)
-        print(jsondata.text)
         return jsondata.text
 
     def writePromptString(self):
@@ -76,5 +74,5 @@ class API():
 if(__name__=="__main__"):
     a = API()
     #print(a.parseChats()[0])
-    a.readMessage(a.findChatByName("Stephen"))
+    a.readMessage(a.findChatByMemberName("Stephen"))
     #print(a.getUserData())
