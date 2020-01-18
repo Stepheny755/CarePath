71bed5b4f6b2596598c3a617033705d92f45ff3a
Stephen Yang <stepheny755@gmail.com>
2020-01-18 01:17:01 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,14 @@ app = Flask('main')
 def main():
     return 'hello world'
 
+<<<<<<< Updated upstream
+=======
+@app.route('/postmethod')
+def post_json():
+    temp = {'id':123456789,'name':'CarePath','value':'test'}
+    return json.dumps(temp)
+
+
+
+>>>>>>> Stashed changes
 Flask.run(app,host="167.99.186.154",port=8080)
