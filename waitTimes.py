f5344ba3ef950d81cc6dd9c0dae81ab17a5608bb
Stephen Yang <stepheny755@gmail.com>
2020-01-18 10:47:16 -0500

    added hospital average wait times

--- waitTimes.py
+++ waitTimes.py
@@ -1,4 +1,4 @@
-import requests,json
+import requests,json,csv,os
 from xml.dom import minidom
 
 class waitTimes():
@@ -8,18 +8,29 @@ class waitTimes():
     def __init__(self):
         pass
 
+    def readWaitTimes(self):
+        readList = []
+        filename="waittimes.csv"
+        path="resources"
+        with open(os.path.join(path,filename).strip(),'r') as r:
+            readList = list(csv.reader(r,delimiter=','))
+        return readList
 
-    def getWaitTimeTable(self):
+    def getHospitalList(self):
         payload={'language':'en-CA'}
         table = requests.post(self.uri,payload);
         xmlDocument = minidom.parseString(table.text)
         itemList = xmlDocument.getElementsByTagName('string')
         dataList = itemList[0].firstChild.nodeValue
-        for value in json.loads(dataList):
-            print(value)
+        return json.loads(dataList)
 
+    def parseHospitalList(self,hospitalList):
+        for i in hospitalList:
+            print(i)
 
 if(__name__=="__main__"):
     w=waitTimes()
-    w.getWaitTimeTable()
+    d=w.getHospitalList()
+    w.parseHospitalList(d)
+    print(w.readWaitTimes())
     #w.testRequest()
