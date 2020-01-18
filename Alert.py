9da0901bdc18a5060e8cde12a1417741579e3d09
Navidur Rahman <navid2001@gmail.com>
2020-01-18 09:47:41 -0500

    Create Alert.py

---
+++ Alert.py
@@ -0,0 +1,27 @@
+from datetime import *
+
+class Alert:
+    
+    # Initializer
+    def __init__(self, name, times, finalDate, dosage):
+        self.name = name
+        self.dates = dates
+        self.times = times
+        self.finalDate = finalDate
+        self.dosage = dosage
+        self.currentTime = now()
+        self.currentDate = today()
+
+    def checkTime(self):
+        if currentTime.hour in times[currentDate] and currentDate < finalDate:
+            doAlert()
+
+    def doAlert(self):
+        alertBool = True
+        #pass this through
+        alertBool = False
+            
+            
+
+
+    
