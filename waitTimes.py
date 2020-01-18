e9e2620481e5db2064aea7d622ce699c4500ccf4
Stephen Yang <stepheny755@gmail.com>
2020-01-18 11:15:30 -0500

    updated waittimes

--- waitTimes.py
+++ waitTimes.py
@@ -6,7 +6,7 @@ class waitTimes():
     uri = 'http://hqontario.ca/webservices/wsWaitTimes.asmx/getTableDataAllHospitals'
 
     def __init__(self):
-        pass
+        self.waitTimeList = []
 
     def readWaitTimes(self):
         readList = []
@@ -14,7 +14,15 @@ class waitTimes():
         path="resources"
         with open(os.path.join(path,filename).strip(),'r') as r:
             readList = list(csv.reader(r,delimiter=','))
+        self.waitTimeList = readList
         return readList
+
+    def getHospitalWaitTime(self,name):
+        if not self.waitTimeList:
+            self.readWaitTimes()
+        for i in self.waitTimeList:
+            if(name==i[0]):
+                return i[1]
 
     def getHospitalList(self):
         payload={'language':'en-CA'}
@@ -32,5 +40,5 @@ if(__name__=="__main__"):
     w=waitTimes()
     d=w.getHospitalList()
     w.parseHospitalList(d)
-    print(w.readWaitTimes())
+    w.getHospitalWaitTime("Sensenbrenner Hospital")
     #w.testRequest()
