ac9fac250bf51f69609ec8853303a6e2fbe58d98
Stephen Yang <stepheny755@gmail.com>
2020-01-18 09:54:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ def main():
 @app.route('/sendmethod')
 def post_json():
     temp = {'id':123456789,'name':'CarePath','value':'test'}
-    searchurl = 'https://jsonplaceholder.typicode.com/todos/1'
-    temp2 = requests.get(searchurl)
     return json.dumps(temp)
 
 
 
-Flask.run(app,host="167.99.186.154",port=8080)
+Flask.run(app,host="167.99.186.154")
