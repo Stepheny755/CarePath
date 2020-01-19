b6f331869080b79201a2724b3f50f56fe638ea90
stepheny755 <stepheny755@gmail.com>
2020-01-19 03:38:01 +0000

    updated transport.py

--- transport.py
+++ transport.py
@@ -1,9 +1,12 @@
 import json,requests
+from flask import Flask,request
+
+app = Flask(__name__)
 
 @app.route('/postmethod',methods=['POST'])
 def post_json():
     data=request.form
-    print(data)
+    print(data.text)
     return data
 
 Flask.run(app,host="167.99.186.154")
